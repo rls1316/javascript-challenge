5f8dfaf38dab3e649a5c19e08c7e4d1333cd02eb
rls1316 <80739270+rls1316@users.noreply.github.com>
2021-08-26 13:43:25 -0700

    saving point

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,22 @@
-// from data.js
+// from data.js -- populate table
 var tableData = data;
 
-// YOUR CODE HERE!
+// select the table body
+var tbody = d3.select("tbody");
+
+// create function to build table, set values and print from data
+function buildTable(data) {
+    tbody.html("");
+
+    data.forEach((sighting) => {
+        var row = tbody.append("tr");
+    
+    // iterate thru keys and values
+    Object.entries(sighting).forEach(([key, value]) => console.log(key, value);
+        var cell = row.append("td") cell.text(value)
+    )});
+}
+
+// Filter table button
+
+
